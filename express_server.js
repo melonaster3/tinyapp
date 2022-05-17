1f1808560ea6c0cd746b0ff646875a8c4a1bba25
Sang Lee <melonaster@gmail.com>
2022-05-17 01:43:34 +0000

    After URL shortening part1

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine", "ejs"); // Express app use EJS as template 
 
@@ -9,14 +11,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls", (req, res) => {
+app.get("/urls/new", (req, res) => { // When the user inputs new url
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => { // AFter input of new URL, server sends back okay
+  console.log(req.body);
+  res.send("Okay");
+})
+
+app.get("/urls", (req, res) => { // Able to see the list of URLs posted
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL : req.params.shortURL, longURL : req.params.longURL};
-  res.render("urls_show", templateVars);
+  res.render("urls_show", templateVars); // Able to see the shorten URLs
 })
 
 app.get("/", (req, res) => { 
@@ -28,9 +39,18 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  res.json(urlDatabase); // Convert the URL data base into JSON
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send("<html><body>Hello <b>World</b></body></html>\n"); //Example of Express format
 });
+
+function generateRandomString() { // generate random 6 number character string
+  let string = '';
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < characters.length; i++) {
+    string += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return string;
+}
